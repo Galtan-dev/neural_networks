c43bdf91999159722f6efda27685e1317d8b83cf
Jakub Seiner <jakub.seiner@seznam.cz>
2024-03-06 10:03:01 +0100

    new lessons

---
+++ lekce_3_1/runCommands.bat
@@ -0,0 +1,15 @@
+python mnist_regularization.py
+python mnist_regularization.py --hidden_layer_sizes="400,400"
+python mnist_regularization.py --weight_decay=0.01
+python mnist_regularization.py --weight_decay=0.1
+python mnist_regularization.py --weight_decay=0.3
+python mnist_regularization.py --weight_decay=0.5
+python mnist_regularization.py --dropout=0.1
+python mnist_regularization.py --dropout=0.3
+python mnist_regularization.py --dropout=0.5
+python mnist_regularization.py --label_smoothing=0.1
+python mnist_regularization.py --label_smoothing=0.3
+python mnist_regularization.py --label_smoothing=0.5
+python mnist_regularization.py --dropout=0.3 --label_smoothing=0.3
+python mnist_regularization.py --dropout=0.1 --label_smoothing=0.1
+python mnist_regularization.py --dropout=0.1 --label_smoothing=0.3
